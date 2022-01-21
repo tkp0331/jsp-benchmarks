ed1f88da462ac88715b9b0d5a9bb2f02362cec00
鈴木貴大 <merioda.seven.24@gmail.com>
2022-01-21 18:16:11 +0900

    first commit

---
+++ jsp_benchmarks/__init__.py
@@ -0,0 +1,3 @@
+__version__ = '0.1.1'
+
+from .instance_loader import JSPMetaData, JSPInstance, JSPInstanceLoader
---
+++ tests/test_jsp_benchmarks.py
@@ -0,0 +1,5 @@
+from jsp_benchmarks import __version__
+
+
+def test_version():
+    assert __version__ == '0.1.0'
